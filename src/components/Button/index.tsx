0991698c68b4e2d4b018e9f2af40c01361a3c0b8
dev-mubasshir <shkhaider2015@gmail.com>
2021-12-07 17:07:04 +0500

    some changes

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -9,23 +9,97 @@ interface propType {
     disable?: boolean | undefined
     outlined?: boolean
 }
-
-
+interface IStyle {
+    btnRoot: {
+        display: string,
+        justifyContent: string,
+        borderRadius: string,
+        height: string,
+        paddingLeft: string,
+        paddingRight: string,
+        transition: string
+    },
+    btnOutlined: {
+        backgroundColor: string,
+        border: string,
+        color: string
+    },
+    btnContained: {
+        backgroundColor: string
+    },
+    btnDisable: {
+        backgroundColor: string
+    },
+    btnIcon: {
+        fill: string,
+        width: string,
+        height: string,
+        marginTop: string,
+        marginBottom: string
+    },
+    btnTextRoot: {
+        marginTop: string,
+        marginBottom: string
+    },
+    btnText: {
+        fontSize: string,
+        fontWeight: string,
+        paddingLeft: string
+    }
+}
+const style:IStyle = {
+    btnRoot: {
+        display: 'flex',
+        justifyContent: 'center',
+        borderRadius: '10px',
+        height: '40px',
+        paddingLeft: '10px',
+        paddingRight: '10px',
+        transition: 'background-color .2s'
+    },
+    btnOutlined: {
+        backgroundColor: '#ffffff',
+        border: '1px solid #1B4238',
+        color: '#1B4238'
+    },
+    btnContained: {
+        backgroundColor: '#FFBF30'
+    },
+    btnDisable: {
+        backgroundColor: '#d3b575'
+    },
+    btnIcon: {
+        fill: '#000000',
+        width: '14px',
+        height: '12px',
+        marginTop: 'auto',
+        marginBottom: 'auto'
+    },
+    btnTextRoot: {
+        marginTop: 'auto',
+        marginBottom: 'auto'
+    },
+    btnText: {
+        fontSize: '14px',
+        fontWeight: 'bold',
+        paddingLeft: '2px'
+    }
+}
 
 const GoldiButton = ({ text, Icon, onClick, disable, outlined }:propType) => {
 
     return <div  className={`btn-root ${outlined ? "btn-outlined" : "btn-contained"} 
-                    ${disable ? "disable" : ""} 
-                    `} onClick={() => disable ? '' : onClick() } >
-        {
-            Icon && (<div className="btn-icon-root" >
-            <Icon className="btn-icon" />
-        </div>)
-        }
-        <div className="btn-text-root" >
-            <span className="btn-text" > {text} </span>
-        </div>
-    </div>
+    ${disable ? "disable" : ""} 
+    `} onClick={() => disable ? '' : onClick() } >
+{
+Icon && (<div className="btn-icon-root" >
+<Icon className="btn-icon" />
+</div>)
+}
+<div className="btn-text-root" >
+<span className="btn-text" > {text} </span>
+</div>
+</div>
 }
 
 export default GoldiButton;
--- src/stories/Button.stories.tsx
+++ src/stories/Button.stories.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { Diet } from "../Static/Icons";
 
-import { Button } from './Button';
+import Button from '../components/Button';
 
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
@@ -14,28 +15,27 @@ export default {
 } as ComponentMeta<typeof Button>;
 
 // More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
-const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
+const Template: ComponentStory<typeof Button> = (args) => <Button {...args} text="Click Me" />;
 
-export const Primary = Template.bind({});
+export const Contained = Template.bind({});
 // More on args: https://storybook.js.org/docs/react/writing-stories/args
-Primary.args = {
-  primary: true,
-  label: 'Button',
+Contained.args = {
+  outlined: false,
+  Icon: Diet,
+  text: "Click Me"
 };
 
-export const Secondary = Template.bind({});
-Secondary.args = {
-  label: 'Button',
-};
+// export const Outlined = Template.bind({});
+// Outlined.args = {
+//   outlined: true
+// };
 
-export const Large = Template.bind({});
-Large.args = {
-  size: 'large',
-  label: 'Button',
-};
+// export const IconButton = Template.bind({});
+// IconButton.args = {
+//   Icon: Diet
+// };
 
-export const Small = Template.bind({});
-Small.args = {
-  size: 'small',
-  label: 'Button',
-};
+// export const Disable = Template.bind({});
+// Disable.args = {
+//   disable: true
+// };
