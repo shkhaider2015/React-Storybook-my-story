ff97cc79e6107119492545f2ff100507c7f502e4
dev-mubasshir <shkhaider2015@gmail.com>
2021-12-10 15:58:31 +0500

    sdsdsds

---
+++ src/components/Dropdown/Elements/RotateY/index.tsx
@@ -0,0 +1,16 @@
+import "./index.css";
+
+const RotateY = () => {
+    return <div className="dropdown-rotatey">
+    <span className="title" >RotateY</span>
+    <div className="dropdown-rotatey-menu dropdown-rotatey-menu--animated">
+      <div className="dropdown-rotatey-item">Item 1</div>
+      <div className="dropdown-rotatey-item">Item 2</div>
+      <div className="dropdown-rotatey-item">Item 3</div>
+      <div className="dropdown-rotatey-item">Item 4</div>
+      <div className="dropdown-rotatey-item">Item 5</div>
+    </div>
+  </div>
+}
+
+export default RotateY;
